55df23e386838ece68156cb79d33f8dcfac85d22
Steffi-Patel <122414146+Steffi-Patel@users.noreply.github.com>
2023-11-02 18:44:27 -0400

    assignmnets rendering

--- src/Kanbas/Courses/Assignments/index.js
+++ src/Kanbas/Courses/Assignments/index.js
@@ -1,42 +1,40 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { FaFileLines, FaCircleCheck, FaEllipsisVertical, FaTrash } from "react-icons/fa6";
+import db from "../../Database";
 import "./assignments.css";
+import { FaFileLines, FaCircleCheck, FaEllipsisVertical } from "react-icons/fa6";
 
 function Assignments() {
   const { courseId } = useParams();
-  const assignments = useSelector((state) => state.assignments);
-  const dispatch = useDispatch();
-  const courseAssignments = assignments.filter((assignment) => assignment.course === courseId);
-
-  const deleteAssignment = (id) => {
-    const confirmation = window.confirm("Are you sure you want to delete this assignment?");
-    if (confirmation) {
-      dispatch({ type: 'DELETE_ASSIGNMENT', payload: id });
-    }
-  };
+  const assignments = db.assignments;
+  const courseAssignments = assignments.filter(
+    (assignment) => assignment.course === courseId
+  );
 
   return (
     <div>
       <h5>Assignments for course {courseId}</h5>
 
-      <div className="d-flex mb-2 mt-5">
+      <div class="d-flex mb-2 mt-5">
         <input
           type="text"
-          className="form-control"
+          class="form-control"
           style={{ width: "250px" }}
           title="Type the name of the assignment"
           placeholder="Search for Assignments"
         />
-        <div className="flex-grow-1"></div>
-        <button type="button" className="btn btn-light ml-2">
+
+        <div class="flex-grow-1"></div>
+
+        <button type="button" class="btn btn-light ml-2">
           + Group
         </button>
-        <Link to={`/Kanbas/Courses/${courseId}/AssignmentEditor`} className="btn btn-danger ml-2">
+
+        <button type="button" class="btn btn-danger ml-2">
           + Assignment
-        </Link>
-        <select className="ml-2">
+        </button>
+
+        <select class="ml-2">
           <option selected value="Edit Assignment Dates">
             +
           </option>
@@ -46,25 +44,24 @@ function Assignments() {
 
       <div className="list-group border-left-green">
         {courseAssignments.map((assignment) => (
-          <div key={assignment._id} className="list-group-item d-flex justify-content-between">
-            <Link to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}>
-              <div className="d-flex align-items-center justify-content-between">
-                <div className="d-flex align-items-center">
-                  <FaEllipsisVertical className="mr-2" />
-                  <FaEllipsisVertical className="mr-2" />
-                  <FaFileLines className="text-success mr-3" />
-                </div>
-                {assignment.title}
-                <div className="d-flex align-items-center">
-                  <FaCircleCheck className="text-success mr-2" />
-                  <FaEllipsisVertical />
-                </div>
+          <Link
+            key={assignment._id}
+            to={`/Kanbas/Courses/${courseId}/Assignments/${assignment._id}`}
+            className="list-group-item"
+          >
+            <div className="d-flex align-items-center justify-content-between">
+              <div className="d-flex align-items-center">
+                <FaEllipsisVertical className="mr-2" />
+                <FaEllipsisVertical className="mr-2" />
+                <FaFileLines className="text-success mr-3" />
               </div>
-            </Link>
-            <button onClick={() => deleteAssignment(assignment._id)} className="btn btn-danger">
-              <FaTrash />
-            </button>
-          </div>
+              {assignment.title}
+              <div className="d-flex align-items-center">
+                <FaCircleCheck className="text-success mr-2" />
+                <FaEllipsisVertical />
+              </div>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
